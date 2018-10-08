3e5fa5c5a9eb0f8bd3369533ef8d80969cc9a98f
Tomas Mufume <Tomas Mufume@TM>
2018-10-08 19:49:30 +0200

    Painel Cliente

--- src/Visao/paineis/PnCliente.java
+++ src/Visao/paineis/PnCliente.java
@@ -11,12 +11,12 @@ import java.awt.Color;
  *
  * @author TM
  */
-public class PnEstoque extends javax.swing.JPanel {
+public class PnCliente extends javax.swing.JPanel {
 
     /**
      * Creates new form PnVenda
      */
-    public PnEstoque() {
+    public PnCliente() {
         initComponents();
         jScrollPane1.getViewport().setBackground(Color.white );
     }
@@ -49,9 +49,9 @@ public class PnEstoque extends javax.swing.JPanel {
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel1.setText("ESTOQUE");
+        jLabel1.setText("CLIENTE");
 
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Visao/paineis/Trolley_100px.png"))); // NOI18N
+        jLabel3.setIcon(new javax.swing.ImageIcon("C:\\Users\\Tomas Mufume\\Pictures\\User Groups_100px.png")); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -67,7 +67,7 @@ public class PnEstoque extends javax.swing.JPanel {
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(13, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel3)
@@ -132,11 +132,11 @@ public class PnEstoque extends javax.swing.JPanel {
 
             },
             new String [] {
-                "Código", "Produto", "Quantidade", "Tipo de Produto", "Preço de Compra", "Preço de Venda", "Fornecedor", "Data"
+                "Código", "Nome", "Morada", "Email"
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, true, false, false, true
+                false, false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
@@ -160,7 +160,7 @@ public class PnEstoque extends javax.swing.JPanel {
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 420, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 507, Short.MAX_VALUE)
         );
 
         rSMetroTextPlaceHolder1.setForeground(new java.awt.Color(51, 51, 51));
